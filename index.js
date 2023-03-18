521b9589ac074740b1830731fde55fb4c752580d
Jeffchua96 <jeffersonchua@pursuit.org>
2023-03-17 21:00:30 -0400

    LAB HIGHER DONE

--- index.js
+++ index.js
@@ -12,10 +12,14 @@
  *  find([1, 2, 3], (element) => element < 0);
  *  //> undefined
  */
+
 function find(array, callback) {
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)) {
+      return element;
+    }
   }
+  return undefined;
 }
 
 /**
@@ -32,12 +36,17 @@ function find(array, callback) {
  *  filter([1, 2, 3], (element) => element < 0);
  *  //> []
  */
+
 function filter(array, callback) {
-  const result = [];
-  for (let element of array) {
-    // Write your code here.
-  }
-  return result;
+const result = [];
+for (let element of array) {
+  // Write your code here.
+  if(callback(element)){
+   result.push(element)
+}
+}
+return result;
+  
 }
 
 /**
@@ -54,13 +63,17 @@ function filter(array, callback) {
  *  map([], (element) => element < 0);
  *  //> []
  */
+
 function map(array, callback) {
   const result = [];
   for (let element of array) {
     // Write your code here.
-  }
+    
+      result.push(callback(element))
+    }
   return result;
 }
+
 
 /**
  * Does not return anything. Passes each element of the array into the callback along with the index and the array, in that order.
@@ -75,11 +88,14 @@ function map(array, callback) {
  *  //> 20 1 3
  *  //> 30 2 3
  */
-function forEach(array, callback) {
-  for (let i = 0; i < array.length; i++) {
-    // Write your code here.
-  }
-}
+
+    function forEach(array, callback) {
+      for (let i = 0; i < array.length; i++) {
+        //Write your code here.
+        callback(array[i], i, array);
+      }
+    }
+  
 
 // Do not change the code below this line.
 module.exports = { find, filter, map, forEach };
